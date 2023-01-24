6cd7536e769d90f8cccbd97efc680ebd03422efa
Jiewen Tan <jwtan@google.com>
2023-01-24 01:39:24 +0000

    [Functionalization] Fix SPMD tests
    Summary:
    This pull request tries to fix three SPMD tests that fails with Functionalization.
    1. test_optimizer_step_with_sharding: Currently SPMD relies on tensor.add_ to
    populate sharding spec for weights across different iterations. However, Functionalization
    removes mutations and therefore this method is not working anymore. To reconcile that,
    the SPMD team is trying to figure out a new place to store the sharding spec. Potentially,
    the DTensor or XLAShardedTensor such that the python object is the ultimate source of truth and
    can be kept intact regardless any dispatcher magic. We shouldn't need propagation in this case.
    2. test_inplace_add_with_sharding: See above.
    3. test_model_weight_metrics: Before Functionalization, model.weight is an at::tensor stored
    in the XLATensor, but it's an IRValue now. Therefore, the test no longer holds true.
    
    Test Plan:
    CI.

--- torch_xla/csrc/init_python_bindings.cpp
+++ torch_xla/csrc/init_python_bindings.cpp
@@ -1501,9 +1501,12 @@ void InitXlaModuleBindings(py::module m) {
       // If the at::Tensor data is not present, we need to re-download the
       // tensor from the physical device to CPU. In that case, the value
       // must be present on the backend device.
-      XLA_CHECK(xtensor->GetXlaData() != nullptr &&
-                xtensor->CurrentDataHandle()->HasValue())
-          << "Cannot shard tensor. Data not present on any device.";
+      {
+        XLAGraphExecutor::Get()->DeviceBarrier(xtensor->GetDevice());
+        XLA_CHECK(xtensor->GetXlaData() != nullptr &&
+                  xtensor->CurrentDataHandle()->HasValue())
+            << "Cannot shard tensor. Data not present on any device.";
+      }
       std::vector<XLATensorPtr> xla_tensors{xtensor};
       cpu_tensor = XLAGraphExecutor::Get()->GetTensors(&xla_tensors)[0];
     }
