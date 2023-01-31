99b64fc7d25a3904d538abb0860d555e87aa8115
Yeounoh Chung <yeounoh@google.com>
2023-01-31 14:19:20 -0800

    [SPMD] temporary functionalization fix

--- torch_xla/csrc/xla_graph_executor.cpp
+++ torch_xla/csrc/xla_graph_executor.cpp
@@ -463,8 +463,6 @@ XLAGraphExecutor::SyncTensorCollection XLAGraphExecutor::CollectSyncTensors(
     return coll;
   }
 
-  std::cout << "*** CollectSyncTensors..." << std::endl;
-
   std::vector<at::Tensor> at_tensors;
   std::vector<std::string> devices;
   std::vector<XLATensor::ShardingSpecPtr> shardings;
@@ -485,7 +483,6 @@ XLAGraphExecutor::SyncTensorCollection XLAGraphExecutor::CollectSyncTensors(
           !tensors[i]->data()->view->IsUpToDate()))) {
       torch::lazy::Value ir_value = tensors[i]->CurrentIrValue();
       if (ir_value) {
-	std::cout << "*** ir_value: " << ir_value->ToString() << std::endl;
         if (ShouldSyncIrValue(ir_value)) {
           // Add only tensors which need to be synced.
           coll.hash = torch::lazy::HashCombine(coll.hash, ir_value.hash());
